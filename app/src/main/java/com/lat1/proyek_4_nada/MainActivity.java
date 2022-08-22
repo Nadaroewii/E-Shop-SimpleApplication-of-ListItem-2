00db9821d82a7f358a1e22ef3d23ceea35edea7e
Nadaroewii <nadaroewii@gmail.com>
2022-08-22 23:24:21 +0700

    update

---
+++ app/src/main/java/com/lat1/proyek_4_nada/MainActivity.java
@@ -0,0 +1,118 @@
+package com.lat1.proyek_4_nada;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.lat1.proyek_4_nada.adapter.ItemAdapter;
+import com.lat1.proyek_4_nada.constant.Constants;
+import com.lat1.proyek_4_nada.models.Item;
+import com.lat1.proyek_4_nada.models.Result;
+import com.lat1.proyek_4_nada.service.APIService;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvItem;
+    private FloatingActionButton fabAdd;
+
+    ArrayList<Item> items = new ArrayList<Item>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvItem = findViewById(R.id.rv_item);
+        rvItem.setLayoutManager(new LinearLayoutManager(this));
+        rvItem.setHasFixedSize(true);
+
+        fabAdd = findViewById(R.id.fab_add);
+
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        MainActivity.this,
+                        AddAndUpdate.class);
+
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadAllData();
+
+    }
+
+    public void loadAllData() {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Silahkan Tunggu ...");
+        progressDialog.show();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Constants.URL_API)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        APIService apiService = retrofit.create(APIService.class);
+
+        final Call<Result> result = apiService.getAll(Constants.TOKEN);
+
+        result.enqueue(new Callback<Result>() {
+            @Override
+            public void onResponse(Call<Result> call, Response<Result> response) {
+                progressDialog.dismiss();
+                Log.d("MainActivity", response.body().toString());
+
+                Result jsonResult = response.body();
+
+                items = jsonResult.getItems();
+
+                ItemAdapter itemAdapter = new ItemAdapter(MainActivity.this);
+
+                rvItem.setAdapter(itemAdapter);
+
+                if (items != null) {
+                    itemAdapter.setListItems(items);
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(Call<Result> call, Throwable t) {
+                progressDialog.dismiss();
+
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/lat1/proyek_4_nada/constant/Constants.java
@@ -0,0 +1,8 @@
+package com.lat1.proyek_4_nada.constant;
+
+public class Constants {
+
+    public static final String URL_API = "https://vsga.herokuapp.com/";
+    public static final String TOKEN = "tokenpunyaNada";
+
+}
